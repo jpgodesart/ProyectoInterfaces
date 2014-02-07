30dcbbb7502144d80e85ec553ac434600cc26653
Win7 <Win7@Win7-PC>
2014-02-07 18:58:34 +0100

    2

--- web/js/playas.js
+++ web/js/playas.js
@@ -1,2 +1,15 @@
-
+$(document).ready(function() {
+    $("#botonvideo").click(function() {
+        $(this).css("display", "none");
+        $("#img1").css("display", "none");
+        $("#vid1").css("display", "block");
+        $("#botonfoto").css("display", "block");
+    });
+    $("#botonfoto").click(function() {
+        $(this).css("display", "none");
+        $("#img1").css("display", "block");
+        $("#vid1").css("display", "none");
+        $("#botonvideo").css("display", "block");
+    });
+});
 
